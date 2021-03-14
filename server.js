e7f4bb3e35195a820ab77eb15ae6fda3c8bf1984
Tran Son Hai <harefx.dev@gmail.com>
2021-03-14 23:06:37 +0700

    add body-parser to parse json body

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-app.use(cors())
+app.use(cors());
+app.use(bodyParser.json());
 
-app.get("/messages", (req,res) => {
-  const messages = ["yes", "hi", "It's working", "haha"]
+let messages = ["yes", "hi", "It's working", "haha"];
 
-  res.send(messages)
+app.get("/messages", (req, res) => {
+  res.send(messages);
+})
+
+app.post('/messages', (req, res) => {
+  let msg = req.body;
+  messages.push(msg.message);
+  res.json(msg);
+  console.log(messages);
 })
 
 app.listen(port, () => console.log(`app running on port ${port}!`))
