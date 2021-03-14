2ad72fde4750102110f1040b8e254a77f4549682
Tran Son Hai <harefx.dev@gmail.com>
2021-03-14 19:22:26 +0700

    set up messages route

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+
+app.get("/messages", (req,res) => {
+  const messages = ["hello", "hi", "It's working", "haha"]
+
+  res.send(messages)
+})
+
+app.listen(port, () => console.log(`app running on port ${port}!`))
