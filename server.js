de4047764c5e68b5fdffc95083ae532c8af4f868
Tran Son Hai <harefx.dev@gmail.com>
2021-03-15 02:17:35 +0700

    add register route

--- server.js
+++ server.js
@@ -7,20 +7,32 @@ const port = 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
-let messages = ["yes", "hi", "It's working", "haha"];
+let messages = [{text: "yes", user: "Jim"}, {user: "Helen", text: "hi"}];
+let users = [{ userName: "Jim", userId: 1}, { userName: "Helen", userId: 0 }];
 
 app.get("/messages", (req, res) => {
   res.send(messages);
 })
 
 app.post('/messages', (req, res) => {
-  let msg = req.body;
-  messages.push(msg.message);
+  const userId = req.header('Authorization')
+  const user = users[userId]
+  let msg = { user: user.userName, text: req.body.message }
+  messages.push(msg);
   res.json(msg);
 })
 
 app.get('/messages/:id', (req, res) => {
   res.send(messages[Number(req.params.id)]);
+})
+
+app.post('/register', (req, res) => {
+  let registerData = req.body;
+
+  let newIndex = users.push(registerData);
+  registerData.id = newIndex - 1;
+
+  res.json(registerData)
 })
 
 app.listen(port, () => console.log(`app running on port ${port}!`))
